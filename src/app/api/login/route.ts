cadd19a5f6e365726a3e4f2a476c420b33a914c0
Henrique <henrique.asouza@outlook.com>
2024-06-08 12:01:19 -0300

    feat: add api route for login

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,30 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+const requestBackend = async (username: string, password: string) => {
+  const url = process.env.API_URL as string;
+  const response = await fetch(`${url}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
+  return response;
+};
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const data = await requestBackend(body.username, body.password);
+  const jsonData = await data.json();
+
+  if (data.status === 200) {
+    const cookieStore = cookies();
+    cookieStore.set("acess-token", jsonData.access_token);
+  }
+
+  return NextResponse.json(jsonData, { status: data.status });
+}
